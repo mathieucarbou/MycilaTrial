532a188089d5652a137d89b1ab9f36190ec9bca2
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-04-21 13:19:49 +0200

    v1.0.1

--- src/MycilaTrial.h
+++ src/MycilaTrial.h
@@ -7,10 +7,10 @@
 #include <Preferences.h>
 #include <stdint.h>
 
-#define MYCILA_TRIAL_VERSION "1.0.0"
+#define MYCILA_TRIAL_VERSION "1.0.1"
 #define MYCILA_TRIAL_VERSION_MAJOR 1
 #define MYCILA_TRIAL_VERSION_MINOR 0
-#define MYCILA_TRIAL_VERSION_REVISION 0
+#define MYCILA_TRIAL_VERSION_REVISION 1
 
 #ifndef MYCILA_TRIAL_DURATION
 #define MYCILA_TRIAL_DURATION 86400 // 1 day in seconds
